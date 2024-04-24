0ec1ac3c749a367dc102abca275c4cfea7d0d480
21kWorlds <21kworlds@gmail.com>
2024-04-24 12:57:25 -0700

    cateringinfo attempt 3

--- components/plasmic/app_starter/PlasmicHomepage.jsx
+++ components/plasmic/app_starter/PlasmicHomepage.jsx
@@ -139,7 +139,7 @@ function PlasmicHomepage__RenderFunc(props) {
                     data-plasmic-name={"button"}
                     data-plasmic-override={overrides.button}
                     className={classNames("__wab_instance", sty.button)}
-                    href={`/catering-info`}
+                    href={`/new-page`}
                   >
                     <div
                       className={classNames(
