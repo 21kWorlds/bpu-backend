e1b12dd026912ec65f303f6ea2c6c93a6a3e0811
21kWorlds <21kworlds@gmail.com>
2024-05-02 20:10:38 -0700

    trying new cateringintake

--- pages/catering-intake/[id].jsx
+++ pages/catering-intake/[id].jsx
@@ -2,24 +2,24 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../components/plasmic/app_starter/PlasmicGlobalContextsProvider";
-import { PlasmicNewPage } from "../components/plasmic/bpu_backend/PlasmicNewPage.jsx";
+import GlobalContextsProvider from "../../components/plasmic/app_starter/PlasmicGlobalContextsProvider";
+import { PlasmicCateringIntakePage } from "../../components/plasmic/bpu_backend/PlasmicCateringIntakePage";
 import { useRouter } from "next/router";
 
-function NewPage() {
-  // Use PlasmicNewPage to render this component as it was
+function CateringIntakePage() {
+  // Use PlasmicCateringIntakePage to render this component as it was
   // designed in Plasmic, by activating the appropriate variants,
   // attaching the appropriate event handlers, etc.  You
   // can also install whatever React hooks you need here to manage state or
   // fetch data.
   //
-  // Props you can pass into PlasmicNewPage are:
+  // Props you can pass into PlasmicCateringIntakePage are:
   // 1. Variants you want to activate,
   // 2. Contents for slots you want to fill,
   // 3. Overrides for any named node in the component to attach behavior and data,
   // 4. Props to set on the root node.
   //
-  // By default, PlasmicNewPage is wrapped by your project's global
+  // By default, PlasmicCateringIntakePage is wrapped by your project's global
   // variant context providers. These wrappers may be moved to
   // Next.js Custom App component
   // (https://nextjs.org/docs/advanced-features/custom-app).
@@ -30,10 +30,10 @@ function NewPage() {
         params={useRouter()?.query}
         query={useRouter()?.query}
       >
-        <PlasmicNewPage />
+        <PlasmicCateringIntakePage />
       </PageParamsProvider__>
     </GlobalContextsProvider>
   );
 }
 
-export default NewPage;
+export default CateringIntakePage;
